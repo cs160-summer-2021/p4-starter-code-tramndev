aaeec4d6f8b955379f711dc81417249b0338338b
Tram Nguyen <tramn@berkeley.edu>
2021-07-26 00:01:23 -0700

    3.9 added

--- draw/urls.py
+++ draw/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<str:room_name>/', views.room, name='room'),
 
+
+    # Deliverable 3.9 @Tram Nguyen & Hiroshi Usui  
+    path('new_interaction', views.new_interaction, name='new_interaction'),
 ]
+
--- draw/views.py
+++ draw/views.py
@@ -8,3 +8,8 @@ def room(request, room_name):
     return render(request, 'draw/room.html', {
         'room_name': room_name
     })
+
+
+# Deliverable 3.9 @Tram Nguyen & Hiroshi Usui  
+def new_interaction(request):
+    return render(request, 'draw/new_interaction.html')
