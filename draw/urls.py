924cab9e764ba2a32d72bde92b5fb5ca8ffbe8df
Admin <admin@Admins-MacBook-Pro-3.local>
2021-07-28 21:47:04 -0700

    added urls

--- draw/urls.py
+++ draw/urls.py
@@ -5,10 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:room_name>/', views.room, name='room'),
+    # path('<str:room_name>/', views.room, name='room'),
 
-
-    # Deliverable 3.9 @Tram Nguyen & Hiroshi Usui  
-    path('new_interaction', views.new_interaction, name='new_interaction'),
+    path('variant1', views.variant1, name='variant1'),
+    path('variant2', views.variant2, name='variant2'),
 ]
 
--- draw/views.py
+++ draw/views.py
@@ -4,12 +4,13 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'draw/index.html')
 
-def room(request, room_name):
-    return render(request, 'draw/room.html', {
-        'room_name': room_name
-    })
+# def room(request, room_name):
+#     return render(request, 'draw/room.html', {
+#         'room_name': room_name
+#     })
 
+def variant1(request):
+    return render(request, 'draw/variant1.html')
 
-# Deliverable 3.9 @Tram Nguyen & Hiroshi Usui  
-def new_interaction(request):
-    return render(request, 'draw/new_interaction.html')
+def variant2(request):
+    return render(request, 'draw/variant2.html')
